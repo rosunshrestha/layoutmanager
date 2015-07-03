49f2fd1edca2b8a88c266e20a90c29aa0b2aa365
shreeramneupane <shreeramneupane0@gmail.com>
2015-07-03 21:56:13 +0545

    LM-2 refactoring spec

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -8,10 +8,7 @@ RSpec.describe StaticController, type: :controller do
     end
 
     it { is_expected.to respond_with 302 }
-
-    it 'redirect to public index' do
-      expect(response).to redirect_to ''
-    end
+    it { is_expected.to redirect_to '' }
   end
 
 end
